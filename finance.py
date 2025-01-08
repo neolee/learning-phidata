3fc947afcc567c308192315cee8a2a98ee41694d
Neo Lee <neo.lee@gmail.com>
2025-01-08 17:19:23 +0800

    new: yahoo financial agent

---
+++ finance.py
@@ -0,0 +1,20 @@
+from phi.agent.agent import Agent
+from phi.tools.yfinance import YFinanceTools
+from llm import model
+
+
+agent = Agent(
+    name="Finance Agent",
+    model=model, # type: ignore
+    tools=[YFinanceTools(stock_price=True,
+                         analyst_recommendations=True,
+                         company_info=True,
+                         company_news=True)],
+    instructions=["Use tables to display data"],
+    show_tool_calls=True,
+    markdown=True,
+)
+
+q = "Summarize analyst recommendations for NVDA"
+
+agent.print_response(q, stream=True)
