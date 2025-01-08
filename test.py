c0fd154416d591239788c3174c2db56be007201e
Neo Lee <neo.lee@gmail.com>
2025-01-08 16:38:19 +0800

    fix: importing and better structure

--- test.py
+++ test.py
@@ -1,12 +1,17 @@
-from phi.agent import Agent, RunResponse
+from phi.agent.agent import Agent
+from phi.run.response import RunResponse
 from phi.model.openai.like import OpenAILike
 
+
+model=OpenAILike(
+    id="qwen2.5-coder-32b-instruct-mlx",
+    api_key="lm-studio",
+    base_url="http://127.0.0.1:1234/v1",
+)
+
 agent = Agent(
-    model=OpenAILike(
-        id="qwen2.5-coder-32b-instruct-mlx",
-        api_key="LM Studio",
-        base_url="http://127.0.0.1:1234/v1",
-    )
+    name="Basic Agent",
+    model=model, #type:ignore
 )
 
 q = "share a 2 sentence horror story"
